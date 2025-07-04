d4b477e7c122b6c6435b614e52d62bd7e5631522
Awomi Paul Ziakede <apziakede51@gmail.com>
2025-07-04 15:31:34 +0100

    template update

--- Constants/SmsTemplates.cs
+++ Constants/SmsTemplates.cs
@@ -8,7 +8,7 @@ namespace BulkSms_Service.Constants
 {
     public class SmsTemplates
     {
-        public const string MessageTemplate = "Hi {0}, This is to inform you that you are among the chosen one to win a BIG price.";
+        public const string MessageTemplate = "Hi {0}, This is to inform you that you are among the chosen ones to win a BIG price.";
         public const string Route = "MKT";
         public const int Type = 1;
     }
